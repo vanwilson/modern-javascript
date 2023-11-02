09100786285fa5dc0d28641ac39466be8bbbf26d
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-02 12:02:38 -0500

    add main.js and mathjs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var moment = require("moment");
+var math = require("mathjs");
+
 console.log("Hello from JavaScript");
 
 var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
 var rightnow = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(rightnow);
+console.log(sqrt(4));
